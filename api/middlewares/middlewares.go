fb9039ed9776d37fbc810dbbb7a5932d3453c016
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2019-09-09 20:23:37 +0100

    add middleware functions

---
+++ api/middlewares/middlewares.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/jherey/blog/api/auth"
+	"github.com/jherey/blog/api/responses"
+)
+
+// SetMiddlewareJSON formats all responses to JSON
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+// SetMiddlewareAuthentication checks for validity of auth token provided
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
