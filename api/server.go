ecd30fc09fce282f9b74252a1a56b06fdc6567f8
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2019-09-14 20:36:42 +0100

    add docker compose file

--- api/server.go
+++ api/server.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/jherey/blog/api/controllers"
-	// "github.com/jherey/blog/api/seed"
+	"github.com/jherey/blog/api/seed"
 	"github.com/joho/godotenv"
 )
 
@@ -24,7 +24,7 @@ func Run() {
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	// seed.Load(server.DB)
+	seed.Load(server.DB)
 
 	server.Run(":8080")
 }
